a8294f87ce0eb1319b22b0b1ab1eef15f0b05db0
Rasmus Bruus <bruus82@gmail.com>
2022-02-11 10:25:02 +0100

    Working with hashes. Puts value from hash to screen

---
+++ Basics/exercise_three.rb
@@ -0,0 +1,19 @@
+starwars = {:starwars_one   => 1999,
+            :starwars_two   => 2002,
+            :starwars_three => 2005,
+            :starwars_four  => 1977,
+            :starwars_five  => 1980,
+            :starwars_six   => 1983,
+            :starwars_seven => 2015,
+            :starwars_eight => 2017,
+            :starwars_nine  => 2019
+}
+puts starwars[:starwars_one]
+puts starwars[:starwars_two]
+puts starwars[:starwars_three]
+puts starwars[:starwars_four]
+puts starwars[:starwars_five]
+puts starwars[:starwars_six]
+puts starwars[:starwars_seven]
+puts starwars[:starwars_eight]
+puts starwars[:starwars_nine]
