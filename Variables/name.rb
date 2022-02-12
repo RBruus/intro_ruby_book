38de759113b7a4b1c125a8fb6f9b7fd02d53ff05
Rasmus Bruus <bruus82@gmail.com>
2022-02-12 17:20:48 +0100

    name.rb with exercise 3 and 4

--- Variables/name.rb
+++ Variables/name.rb
@@ -2,3 +2,18 @@
 puts "What is your name?"
 name = gets.chomp
 puts  "Hello, how are you " + name + "?"
+
+# next exercise number 3 underneath
+
+10.times do
+  puts name
+end
+
+#next exercise number 4 underneath
+
+puts "What is your firstname?"
+first_name = gets.chomp
+puts "What is your lastname?"
+last_name = gets.chomp
+
+puts "Your fullname is " + first_name + " " + last_name
