ceeb5b459de15eb6d2122c3dc051fd4f0b98466c
Rasmus Bruus <bruus82@gmail.com>
2022-05-05 18:42:00 +0200

    exercise 2

---
+++ loops/Exercise2.rb
@@ -0,0 +1,8 @@
+answer = ""
+
+while answer != "STOP" do
+  puts "Please type 'STOP' if you want to discontinue"
+  svar = gets.chomp
+  puts "You didn't type STOP so I continue"
+  answer = gets.chomp
+  end
